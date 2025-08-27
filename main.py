c18cf56b5c4888537432994fa4a31b935876bc67
pabloul787 <pabloul@uc.cl>
2025-08-26 21:29:56 -0400

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hola joaquin, este es un modelo donde estoy probando exportabilidad del virtual enviroment")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
