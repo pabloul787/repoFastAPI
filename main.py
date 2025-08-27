585372e9d02286db35b23e61c0102c4dcd636dd4
pabloul787 <pabloul@uc.cl>
2025-08-27 08:33:14 -0400

    Segundo codigo del API

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,9 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+
+def read_item(item_id: int, q: Union[str, None] = None):
+
+    return {"item_id": item_id, "q": q}
