9f26983b3861d20cd36cbb93a76cbc7a23f2fba4
pabloul787 <pabloul@uc.cl>
2025-10-03 11:43:49 -0300

    Subiendo el main de la api con router

--- apiDemo.py
+++ apiDemo.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
 import pandas as pd
-import apifunctions
+from apifunctions import router_uni as universidad_router
 
 app = FastAPI()
 
-db = pd.read_csv("demoDDBB.csv", sep=";")
-
+app.include_router(universidad_router)
 @app.get("/")
 async def root():
     return "Bienvenido, estos son los ID para consultar información sobre los estudiantes Pablo Uribe --> ID:0\nJoaquin Troncoso --> ID: 1"
     
 
-@app.get("/universidad/{user_id}")
-def main_info(user_id:int):
-    return apifunctions.general_des(user_id)
+#@app.get("/universidad/{user_id}")
+#def main_info(user_id:int):
+   #return apifunctions.general_des(user_id)
