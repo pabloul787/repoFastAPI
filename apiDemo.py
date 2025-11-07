c85071ede7dbe48197caca8cf1c36d4227752736
pabloul787 <pabloul@uc.cl>
2025-11-07 20:09:19 -0300

    mi commit del apiDemo

--- apiDemo.py
+++ apiDemo.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
 import pandas as pd
 from apifunctions import router_uni as universidad_router
+from routes.universidad import router_uni as universidad_router
 from routes.creditos import router as creditos_router
 from routes.banco_central import router as bc_router
-app = FastAPI()
+from contextlib import asynccontextmanager
 
+
+from routes.universidad import crear_db # <-- 2. ADD THIS IMPORT
+
+# --- 3. ADD THE LIFESPAN FUNCTION ---
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Code to run ONCE on server startup
+    print("Server is starting up...")
+    crear_db()
+    yield
+app = FastAPI(lifespan=lifespan)
 app.include_router(universidad_router)
 app.include_router(creditos_router)
 app.include_router(bc_router)   
+
 
 @app.get("/")
 async def root():
